1097600be1b18c75ced8831a6384b0b86e81033e
Alex <alex-goerlitz@gmx.de>
2016-04-25 15:55:17 +0200

    improved regex, started style removal

--- spte.py
+++ spte.py
@@ -4,9 +4,11 @@ import sys
 import os
 import glob
 
-regex = re.compile("[1-9][0-9]*\n"
-    "[0-9][0-9]:[0-5][0-9]:[0-5][0-9],[0-9][0-9][0-9]"
-    " --> [0-9][0-9]:[0-5][0-9]:[0-5][0-9],[0-9][0-9][0-9]\n", re.MULTILINE)
+srt_structure = re.compile("[1-9][0-9]*\n"
+    "[0-9][0-9]:[0-5][0-9]:[0-5][0-9],[0-9][0-9]?[0-9]?"
+    " --> [0-9][0-9]:[0-5][0-9]:[0-5][0-9],[0-9][0-9]?[0-9]?\n", re.MULTILINE)
+opening_tag = re.compile("<[A-Za-z]+>")
+closing_tag = re.compile("<\/[A-Za-z]+>")
 
 
 def main(argv):
@@ -17,8 +19,11 @@ def main(argv):
         inpath = argv[1]
         splitpath = os.path.splitext(inpath)
         if splitpath[1] == ".srt":
-            extract_text(inpath)
-        if splitpath[1] == "":  # folder
+            if argv[2] == "-rs":
+                print("remove")
+                remove_styletags(inpath)
+            #extract_text(inpath)
+        elif splitpath[1] == "":  # folder
             if inpath.endswith("/"):
                 files = glob.glob(inpath + "*srt")
             else:
@@ -26,8 +31,13 @@ def main(argv):
             if not files:  # no files were found
                 print("no .srt files found")
             else:
-                for f in files:
-                    extract_text(f)
+                if argv[2] == "-rs":
+                    for f in files:
+                        remove_styletags(f)
+                        extract_text(f)
+                else:
+                    for f in files:
+                        extract_text(f)
         else:
             print("error reading filepath")
 
@@ -37,14 +47,25 @@ def extract_text(inpath):
     outpath = split[0] + ".txt"
     shutil.copyfile(inpath, outpath)
     f = open(outpath, "r+").read()
-    open(outpath, "w").write(regex.sub("", f))
+    #for line in f:
+     #   print(line)
+    open(outpath, "w").write(srt_structure.sub("", f))
     print("extracted " + outpath)
 
 
-#todo: remove syletags
+def remove_styletags(inpath):
+    split = os.path.splitext(inpath)
+    outpath = split[0] + ".txt"
+    #shutil.copyfile(inpath, outpath)
+    f = open(outpath, "r+").read()
+    open(outpath, "w").write(opening_tag.sub("", f))
+    open(outpath, "w").write(closing_tag.sub("", f))
+
+#todo: dont onverwrite changes, open file/outout once, use multiple regexes
+#todo: better program flow
 #todo: refactor imports
-#<[A-Za-z]+> is start styletag
-#</[A-Za-z]+> is closing styletag
+#todo: proper checking for flags
+
 
 
 if __name__ == "__main__":
