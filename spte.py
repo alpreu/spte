da8c34e5e03823e782a158a00c7f5ae4bb33f1bf
daVincisBane <alex-goerlitz@gmx.de>
2016-05-10 15:19:23 +0200

    style removal now supported

--- spte.py
+++ spte.py
@@ -7,22 +7,22 @@ import glob
 srt_structure = re.compile("[1-9][0-9]*\n"
     "[0-9][0-9]:[0-5][0-9]:[0-5][0-9],[0-9][0-9]?[0-9]?"
     " --> [0-9][0-9]:[0-5][0-9]:[0-5][0-9],[0-9][0-9]?[0-9]?\n", re.MULTILINE)
-opening_tag = re.compile("<[A-Za-z]+>")
-closing_tag = re.compile("<\/[A-Za-z]+>")
+opening_styletag = re.compile("<[A-Za-z]+>")
+closing_styletag = re.compile("<\/[A-Za-z]+>")
 
 
 def main(argv):
-    if "--help" in argv:
+    if "--help" in argv or (len(argv) == 3 and not argv[2] == "-rs"):
         print("Usage: spte [FILE/FOLDER] [OPTION]")
         print(" -rs \t remove styletags")
     else:
         inpath = argv[1]
         splitpath = os.path.splitext(inpath)
         if splitpath[1] == ".srt":
-            if argv[2] == "-rs":
-                print("remove")
-                remove_styletags(inpath)
-            #extract_text(inpath)
+            filedata = create_working_copy(inpath)  # create working copy first
+            if len(argv) == 3 and argv[2] == "-rs":
+                filedata = remove_styletags(filedata)
+            extract_text(filedata)
         elif splitpath[1] == "":  # folder
             if inpath.endswith("/"):
                 files = glob.glob(inpath + "*srt")
@@ -31,42 +31,46 @@ def main(argv):
             if not files:  # no files were found
                 print("no .srt files found")
             else:
-                if argv[2] == "-rs":
+                if len(argv) == 3 and argv[2] == "-rs":
                     for f in files:
-                        remove_styletags(f)
-                        extract_text(f)
+                        filedata = create_working_copy(f)  # create working copy first
+                        filedata = remove_styletags(filedata)
+                        extract_text(filedata)
                 else:
                     for f in files:
-                        extract_text(f)
+                        filedata = create_working_copy(f)  # create working copy first
+                        extract_text(filedata)
         else:
             print("error reading filepath")
 
 
-def extract_text(inpath):
-    split = os.path.splitext(inpath)
-    outpath = split[0] + ".txt"
-    shutil.copyfile(inpath, outpath)
-    f = open(outpath, "r+").read()
-    #for line in f:
-     #   print(line)
-    open(outpath, "w").write(srt_structure.sub("", f))
+def extract_text(filedata):
+    outpath = filedata[0]
+    copyfile = filedata[1]
+    open(outpath, "w").write(srt_structure.sub("", copyfile))
     print("extracted " + outpath)
 
 
-def remove_styletags(inpath):
+def remove_styletags(filedata):
+    outpath = filedata[0]
+    copyfile = filedata[1]
+    open(outpath, "w").write(opening_styletag.sub("", copyfile))
+    copyfile = open(outpath, "r+").read()
+    open(outpath, "w").write(closing_styletag.sub("", copyfile))
+    copyfile = open(outpath, "r+").read()
+    return (outpath, copyfile)
+
+
+#todo: better program flow
+#todo: only import the stuff used
+#todo: improve input handling of flags
+
+def create_working_copy(inpath):
     split = os.path.splitext(inpath)
     outpath = split[0] + ".txt"
-    #shutil.copyfile(inpath, outpath)
-    f = open(outpath, "r+").read()
-    open(outpath, "w").write(opening_tag.sub("", f))
-    open(outpath, "w").write(closing_tag.sub("", f))
-
-#todo: dont onverwrite changes, open file/outout once, use multiple regexes
-#todo: better program flow
-#todo: refactor imports
-#todo: proper checking for flags
-
-
+    shutil.copyfile(inpath, outpath)
+    copyfile = open(outpath, "r+").read()
+    return (outpath, copyfile)
 
 if __name__ == "__main__":
     main(sys.argv)
